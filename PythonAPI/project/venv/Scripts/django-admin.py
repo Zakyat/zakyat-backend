3c1f2cf7271a61d08ae6c27f9d7e9928ebeddb1f
NiyazSafiullin <s.niyaz@mail.ru>
2020-02-19 17:33:53 +0300

    API

---
+++ PythonAPI/project/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\adminzeon\Desktop\Zakyatrt2.0\Zakyat2.0\PythonAPI\project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
