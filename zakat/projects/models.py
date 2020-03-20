86922fd1924a96295949df0e69442fcb83269702
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-03-20 23:05:08 +0300

    Separate Campaign model from Project

--- zakat/projects/models.py
+++ zakat/projects/models.py
@@ -21,6 +21,14 @@ class Request(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Project(models.Model):
+    created_by = models.OneToOneField(Employee, on_delete=models.PROTECT)
+    title = models.CharField(max_length=128)    # TODO: i18n
+    description = models.TextField()    # TODO: i18n
+    # property `campaigns` created with a backref
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Campaign(models.Model):
     request = models.OneToOneField(Request, on_delete=models.SET_NULL, null=True, blank=True)
     created_by = models.OneToOneField(Employee, on_delete=models.PROTECT)
@@ -31,14 +39,4 @@ class Campaign(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     closed_at = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        abstract = True
-    
-class Project(models.Model):
-    created_by = models.OneToOneField(Employee, on_delete=models.PROTECT)
-    title = models.CharField(max_length=128)    # TODO: i18n
-    description = models.TextField()    # TODO: i18n
-    campaigns = models.ArrayField(Campaign, default=[])
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='campaigns')
