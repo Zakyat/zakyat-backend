434899a556362e7701655072da239c6fd8d0a7cd
enghamzasalem <enghamzasalem@yahoo.com>
2020-04-01 00:51:54 +0300

    #8 add news schema and modify projects

--- zakat/projects/schema.py
+++ zakat/projects/schema.py
@@ -16,19 +16,24 @@ class CampaignSchema(DjangoObjectType):
         model = Campaign
 
 class Query(object):
-    request = graphene.Field(RequestSchema)
+    request = graphene.Field(RequestSchema, id=graphene.Int())
     all_requests = graphene.List(RequestSchema)
-    project = graphene.Field(ProjectSchema)
+    project = graphene.Field(ProjectSchema, id=graphene.Int())
     all_projects = graphene.List(ProjectSchema)
-    campaign = graphene.Field(CampaignSchema)
+    campaign = graphene.Field(CampaignSchema, id=graphene.Int())
     all_campaigns = graphene.List(CampaignSchema)
 
     def resolve_all_users(self, info, **kwargs):
         return User.objects.all()
+    def resolve_user(self, info, **kwargs):
+        return User.objects.get(**kwargs)
 
     def resolve_all_projects(self, info, **kwargs):
         return Project.objects.all()
+    def resolve_project(self, info, **kwargs):
+        return Project.objects.get(**kwargs)
 
     def resolve_all_campaigns(self, info, **kwargs):
         return Campaign.objects.all()
-
+    def resolve_Campaign(self, info, **kwargs):
+        return Campaign.objects.get(**kwargs)
--- zakat/zakat/schema.py
+++ zakat/zakat/schema.py
@@ -2,20 +2,15 @@ import graphene
 from graphene_django.debug import DjangoDebug
 
 import accounts.schema
-<<<<<<< HEAD
 import payment.schema
-
+import projects.schema
+import news.schema
 
 class Query(
 	accounts.schema.Query,
 	payment.schema.Query,
-=======
-import projects.schema
-
-class Query(
-	accounts.schema.Query,
 	projects.schema.Query,
->>>>>>> #8 add schema for Projects App
+	news.schema.Query,
 	graphene.ObjectType,
 ):
     debug = graphene.Field(DjangoDebug, name="_debug")
