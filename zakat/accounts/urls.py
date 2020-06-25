c4db9a0e711528f2be2fec2a837b7ca696b1c5b7
Raynur <raynur.khasanov@gmail.com>
2020-06-25 15:12:21 +0300

    Add EmployeeCreateView

--- zakat/accounts/urls.py
+++ zakat/accounts/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('dashboard/staffs/', StaffListView.as_view(), name='staff_list'),
+    path('dashboard/staffs/create', EmployeeCreate.as_view(), name='employee_create_form'),
 
 ]
