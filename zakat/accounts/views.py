d415d2c923802f3a76aefab5a480d551658a5de7
lyf2000 <lyf2000@mail.ru>
2020-06-25 11:57:27 +0300

    add simple logining

--- zakat/accounts/views.py
+++ zakat/accounts/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, redirect, reverse
+from .forms import LoginForm
 
 # Create your views here.
 
 
 def login(request):
-    return render(request, 'accounts/login.html', {})
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    auth_login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
