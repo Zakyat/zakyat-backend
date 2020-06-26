8c9858693022df8b20ee6a658f7064a8d3f4c2bc
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-06-26 10:23:18 +0300

    Realise logout

--- zakat/dashboard/urls.py
+++ zakat/dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
+from . import views
 
 app_name = 'dashboard'
 
 urlpatterns = [
     #Here you write all your paths
     #For example 'staffs/', 'staffs/create'
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
