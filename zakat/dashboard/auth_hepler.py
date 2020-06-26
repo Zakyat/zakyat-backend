05a0cabd265687eb4e57d9bf80ecdc508084cb0e
lyf2000 <lyf2000@mail.ru>
2020-06-26 10:48:47 +0300

    add check user is empl logic(not realised)

---
+++ zakat/dashboard/auth_hepler.py
@@ -0,0 +1,14 @@
+"""
+Module with support logic for auth
+"""
+
+
+def check_is_employee(user) -> bool:
+    """
+    Check is given user has Employee status
+
+    Params:
+        user (object): authenticated user
+
+    """
+    return True
--- zakat/dashboard/views.py
+++ zakat/dashboard/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render
 
+from dashboard.auth_hepler import check_is_employee
 from dashboard.forms import LoginForm
 
 
@@ -19,10 +20,15 @@ def login(request):
             cd = form.cleaned_data
             user = authenticate(username=cd['username'], password=cd['password'])
             if user is not None:
-                auth_login(request, user)
-                messages.info(request, 'Auth-ed successfully.')
+                if not check_is_employee(user):
+                    messages.info(request, f'User {user.username} is not an employee!')
+                else:
+                    auth_login(request, user)
+                    messages.info(request, 'Authenticated successfully!')
             else:
                 messages.error(request, 'User credits are wrong!')
+        else:
+            messages.error(request, 'Given data is not valid!')
     else:
         form = LoginForm()
 
