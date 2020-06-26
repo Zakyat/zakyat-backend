be33ee33f6bfe4493ce4f2d39a533de51aa2afd7
Raynur <raynur.khasanov@gmail.com>
2020-06-26 15:07:40 +0300

    Add permission

--- zakat/dashboard/views.py
+++ zakat/dashboard/views.py
@@ -1,5 +1,5 @@
 from accounts.models import Employee
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.models import Permission
 from django.views.generic.list import ListView
 from django.views.generic import CreateView, UpdateView, DeleteView
@@ -7,18 +7,21 @@ from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 
 
-class StaffListView(LoginRequiredMixin, ListView):
+class StaffListView(LoginRequiredMixin,PermissionRequiredMixin, ListView):
+    permission_required = 'accounts.view_employee'
     model = Employee
     paginate_by = 20
     template_name = 'dashboard/employee/staff_list.html'
 
-class EmployeeCreate(LoginRequiredMixin, CreateView):
+class EmployeeCreate(LoginRequiredMixin,PermissionRequiredMixin, CreateView):
+    permission_required = 'accounts.add_employee'
     model = Employee
     template_name = 'dashboard/employee/employee_create_form.html'
     success_url = '/dashboard/staffs/' #HttpResponseRedirect(reverse('staff_list'))
     fields = '__all__'
 
-class EmployeeEdit(LoginRequiredMixin, UpdateView):
+class EmployeeEdit(LoginRequiredMixin,PermissionRequiredMixin, UpdateView):
+    permission_required = 'accounts.change_employee'
     model = Employee
     fields = '__all__'
     template_name = 'dashboard/employee/employee_edit_form.html'
@@ -29,7 +32,8 @@ class EmployeeEdit(LoginRequiredMixin, UpdateView):
         context['employee'] = get_object_or_404(Employee, pk=self.kwargs['pk'])
         return context
 
-class EmployeeDelete(LoginRequiredMixin, DeleteView):
+class EmployeeDelete(LoginRequiredMixin,PermissionRequiredMixin, DeleteView):
+    permission_required = 'accounts.delete_employee'
     model = Employee
     success_url = reverse_lazy('dashboard:staff_list')
     template_name = 'dashboard/employee/employee_delete_confirm.html'
