0957b80fd639523adc2af26d53079970afed9bb8
Khaliullin Bulat <incidentalname636@gmail.com>
2020-06-26 11:30:28 +0300

    Make partners admin functionality

--- zakat/dashboard/urls.py
+++ zakat/dashboard/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from .views import PartnerList, PartnerDetail
+from .views import PartnerList, PartnerDetail, PartnerDelete, PartnerCreate, PartnerUpdate
 
-
-app_name = 'dashboard'
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('partners/', PartnerList.as_view(), name="partners_list"),
-    path('partners/<int:pk>/', PartnerDetail.as_view(), name="partner_detail")
+    path('partners/create/', PartnerCreate.as_view(), name="partner_create"),
+    path('partners/<int:pk>/', PartnerDetail.as_view(), name="partner_detail"),
+    path('partners/<int:pk>/delete/', PartnerDelete.as_view(), name="partner_delete"),
+    path('partners/<int:pk>/update/', PartnerUpdate.as_view(), name="partner_update"),
 ]
--- zakat/dashboard/views.py
+++ zakat/dashboard/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from partners.models import Partner
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 
 class PartnerList(ListView):
@@ -13,3 +15,20 @@ class PartnerList(ListView):
 class PartnerDetail(DetailView):
     model = Partner
     template_name = 'dashboard/partners/partner_detail.html'
+
+
+class PartnerCreate(CreateView):
+    template_name = 'dashboard/partners/partners_form.html'
+    model = Partner
+    fields = "__all__"
+
+
+class PartnerDelete(DeleteView):
+    model = Partner
+    success_url = reverse_lazy('dashboard:partners_list')
+
+
+class PartnerUpdate(UpdateView):
+    model = Partner
+    fields = "__all__"
+    template_name = 'dashboard/partners/partners_form.html'
