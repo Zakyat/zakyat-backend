d02b50e1d64f866137a0bcdf3120e6fde596c031
lyf2000 <lyf2000@mail.ru>
2020-06-26 09:49:47 +0300

    move code to dashboard

--- zakat/dashboard/urls.py
+++ zakat/dashboard/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 
+from dashboard.views import login
+
 app_name = 'dashboard'
 
 urlpatterns = [
-    #Here you write all your paths
-    #For example 'staffs/', 'staffs/create'
+    # Here you write all your paths
+    # For example 'staffs/', 'staffs/create'
+
+    path('login/', login, name='login'),
 ]
--- zakat/dashboard/views.py
+++ zakat/dashboard/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render
+from dashboard.forms import LoginForm
+from django.contrib import messages
+
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                auth_login(request, user)
+                messages.info(request, 'Auth-ed successfully.')
+            else:
+                messages.error(request, 'User credits are wrong!')
+            return render(request, 'auth/login.html', {'form': form})
+    else:
+        form = LoginForm()
+
+    return render(request, 'auth/login.html', {'form': form})
