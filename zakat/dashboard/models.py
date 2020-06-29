40b714348a2de768b53870d9644b91912aa92085
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-06-29 23:07:00 +0300

    add gathering model without fields

--- zakat/dashboard/models.py
+++ zakat/dashboard/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from accounts.models import Employee
+from .consumers import NotificationConsumer
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
-class Sadaka(models.Model):
-    pass
+
+class Gatherings(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super(Gatherings, self).save(force_insert=False, force_update=False, using=None, update_fields=None)
+
+        layer = get_channel_layer()
+        async_to_sync(layer.group_send)(
+            'notification',
+            {
+                'type': 'notify',
+                'gathering': Gatherings.objects.count()
+            })
