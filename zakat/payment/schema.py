6eec61a1a2e3a502ac7efbc25b91408eaae0cb02
lyf2000 <lyf2000@mail.ru>
2020-06-30 10:14:54 +0300

    add id field in payment schema

--- zakat/payment/schema.py
+++ zakat/payment/schema.py
@@ -10,7 +10,7 @@ class Query(object):
     transaction = graphene.Field(TransactionSchema, id=graphene.Int())
     transactions = graphene.List(TransactionSchema)
 
-    def resolve_transaction(self, info, **kwargs):
-        return Transaction.objects.get(**kwargs)
+    def resolve_transaction(self, info, id=graphene.Int()):
+        return Transaction.objects.get(id=id)
     def resolve_transactions(self, info, **kwargs):
         return Transaction.objects.all()
