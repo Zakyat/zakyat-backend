44f2e289ee30d93488186b1b72ccac52ee9b4db9
Elin Artyom <kevlinsky@outlook.com>
2020-07-05 17:13:10 +0300

    Some changes

--- zakat/dashboard/users/urls.py
+++ zakat/dashboard/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import login
+from .views import login, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
