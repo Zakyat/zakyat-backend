99b7b1b4c7397afb4f475003163bfe028701be5b
Raynur <raynur.khasanov@gmail.com>
2020-07-07 17:47:23 +0300

    Pull up template on delete page

--- zakat/dashboard/employee/urls.py
+++ zakat/dashboard/employee/urls.py
@@ -6,7 +6,6 @@ app_name = 'employee'
 urlpatterns = [
     path('', StaffListView.as_view(), name='staff_list'),
     path('create/', EmployeeCreate.as_view(), name='employee_create_form'),
-    # path('create/', EmployeeCreate.as_view(), name='employee_create_form'),
     path('edit/<int:pk>/', EmployeeEdit.as_view(), name='employee_edit_form'),
     path('delete/<int:pk>/', EmployeeDelete.as_view(), name='employee_delete'),
     path('show_ui/', show_ui),
