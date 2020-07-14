481b8c3866fdfcfdb01b0ec9601a23178ff063d4
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-07-14 15:10:38 +0300

    fix problem on creating user in django-admin

--- zakat/zakat/settings.py
+++ zakat/zakat/settings.py
@@ -169,10 +169,10 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 from PIL import Image
