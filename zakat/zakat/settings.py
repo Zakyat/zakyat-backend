858aa85a880390bd1f2dd0912d009eb482c57dda
lyf2000 <lyf2000@mail.ru>
2020-07-14 19:32:40 +0300

    fix staticfiles not available

--- zakat/zakat/settings.py
+++ zakat/zakat/settings.py
@@ -159,8 +159,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CELERY_BROKER_URL = os.getenv('BROKER_URL', '')
 CELERY_ACCEPT_CONTENT = ['json']
