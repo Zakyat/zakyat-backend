5536d9ff113d79a629773191af37370f99c476b2
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-07-27 14:38:20 +0300

    Make distribute page (not finished yet)

---
+++ zakat/dashboard/sadaka_zakat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import TransactionList, TransactionDetail
+
+app_name = 'sadaka_zakat'
+
+urlpatterns = [
+    path('', TransactionList.as_view(), name='sadaka_zakat_list'),
+    path('<int:pk>/', TransactionDetail.as_view(), name='sadaka_zakat_detail')
+]
