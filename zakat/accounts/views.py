4d748c2200602ae3d000c0eda7d525849be80a23
Azat Gilyazov <azatpscl@gmail.com>
2020-07-31 15:42:39 +0300

    Deleted account.user creating view

--- zakat/accounts/views.py
+++ zakat/accounts/views.py
@@ -2,19 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth import logout, authenticate
 from .models import User
 
-
+# view for tepmplate auth testing
 def testView(request):
-    # if(request.user.is_authenticated):
-    #
-    #     djuser = request.user
-    #     # if(User.objects.get(user=djuser)):
-    #
-    #     zakyatUser = User.objects.create(user=djuser)
-    #     zakyatUser.save()
+
+
 
     return render(request, 'test_auth.html')
 
 
 def user_logout(request):
     logout(request)
-    return render(request, 'test_auth.html')
+    return render(request, 'test_auth.html')   # paste there you redirecting temolate
