c2f1ccedbe2c986c1819adb2362d8fa6a69e6242
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-08-09 19:28:16 +0300

    Create DistributeForm

--- zakat/dashboard/sadaka_zakat/forms.py
+++ zakat/dashboard/sadaka_zakat/forms.py
@@ -3,3 +3,9 @@ from django import forms
 
 class SearchForm(forms.Form):
     name = forms.CharField(required=False)
+
+
+class DistributeForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(DistributeForm, self).__init__(*args, **kwargs)
+        self.fields = args[0]
